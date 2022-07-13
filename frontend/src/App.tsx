a617310264287a1b60583c1ab0733919e0961d68
Kennedy Brito <brito.kennedy10@gmail.com>
2022-07-12 22:16:51 -0300

    criado componente botao

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
-  return(
-    <h1>Titulo 1</h1>
+  return (
+    <>
+      <h1>Titulo 1</h1>
+      <NotificationButton />
+    </>
   )
 }
 
